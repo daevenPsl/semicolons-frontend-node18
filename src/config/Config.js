c1a35660302d9b3ce8a11465b106c9281a7bb808
vineeta_parodkar <vineeta_parodkar@persistent.com>
2023-02-25 09:45:35 +0530

    Adds API services

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,2 +1,3 @@
 //Bundler & RPC configs
 export const BUNDLER_URL = process.env.REACT_APP_BUNDLER_URL;
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
---
+++ src/library/apiService.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+import { API_BASE_URL } from "../config/Config";
+
+export default class APIService {
+  constructor() {
+    let instance = axios.create({
+      baseURL: API_BASE_URL,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    this.apiServiceInstance = instance;
+  }
+
+  async postRequest(headers, apiPathURL, data) {
+    return new Promise((resolve, reject) => {
+      return this.apiServiceInstance
+        .post(apiPathURL, data, {
+          headers,
+        })
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  async getRequest(headers = {}, apiPathURL, params = {}) {
+    return new Promise((resolve, reject) => {
+      return this.apiServiceInstance
+        .get(apiPathURL, { headers, params })
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+}
