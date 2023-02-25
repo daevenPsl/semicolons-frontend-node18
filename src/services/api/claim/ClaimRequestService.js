62c893596b39e8613c73955193dc722d7a44d4ae
vineeta_parodkar <vineeta_parodkar@persistent.com>
2023-02-26 01:28:21 +0530

    Adds claim service

---
+++ src/services/api/claim/ClaimRequestService.js
@@ -0,0 +1,33 @@
+import APIService from "../library/apiService";
+
+const instance = new APIService();
+
+export const getClaim = async () => {
+  try {
+    let response = await instance.getRequest({}, "/claim/retrieve", {});
+    return response;
+  } catch (exception) {
+    console.log("exception in get claim function: " + exception);
+    throw new Error("claim retrieval exception");
+  }
+};
+
+export const saveClaim = async (username, walletAddress, claimType) => {
+  try {
+    let headers = {};
+    let data = {
+      username,
+      walletAddress,
+      claimType,
+    };
+    let response = await instance.postRequest(
+      headers,
+      "/claim/submit",
+      JSON.stringify(data)
+    );
+    return response;
+  } catch (exception) {
+    console.log("Exception occurred during submitting claims:   " + exception);
+    throw new Error("Exception occurred during submitting claims");
+  }
+};
